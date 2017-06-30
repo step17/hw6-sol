c9010a407d0e809aa5c6f8c210f12509b4eb4fdb
Kris Popendorf <krisp@krisp.jp>
2017-07-01 00:44:03 +0900

    use UTF-8 in python

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
+        self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
         self.response.write('こんにちは！')
 
 app = webapp2.WSGIApplication([
