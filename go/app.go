01b51668d6f73e73c943f99cd9b76e5d50f48bdd
Kris Popendorf <krisp@krisp.jp>
2017-07-03 22:18:00 +0900

    pata

--- go/app.go
+++ go/app.go
@@ -1,15 +1,38 @@
 package app
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"strings"
+)
+
+var (
+	templates = template.Must(template.New("base").Funcs(funcMap).ParseGlob("pata.html"))
 )
 
 func init() {
-	http.HandleFunc("/", handlePata)
+	http.HandleFunc("/pata", handlePata)
 }
 
 func handlePata(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello world!\n")
+	a := r.FormValue("a")
+	b := r.FormValue("b")
+	var res string
+	as, bs := strings.Split(a, ""), strings.Split(b, "")
+	if len(as) > 0 && len(bs) > 0 {
+		for i := 0; i < len(as) || i < len(bs); i++ {
+			if i < len(as) {
+				res += as[i]
+			}
+			if i < len(bs) {
+				res += bs[i]
+			}
+		}
+	}
+	err := templates.ExecuteTemplate(w, "pata.html", res)
+	if err != nil {
+		return err
+	}
+	return nil
 }
